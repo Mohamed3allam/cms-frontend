38b011053a0fc6142ac6539c16c8eda51a17420a
Mohamed Abdelrahman <mohamedabdelrahman326@yahoo.com>
2025-09-28 13:35:36 +0300

    tree shaking

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 
-
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ar"],
   },
+  reloadOnPrerender: process.env.NODE_ENV === "development",
   localePath:
     typeof window === "undefined"
-      ? path.resolve("./public/locales")
+      ? path.resolve(process.cwd(), "public/locales")
       : "/locales",
 };
